03e72b2b4953f596c58c4a3a32f213f3f8f0541a
Alfio Parisi <alfioparisi.93@gmail.com>
2017-04-05 16:47:14 +0100

    Add a clock-like display

--- models/pomodoro.js
+++ models/pomodoro.js
@@ -3,8 +3,8 @@ var app = app || {};
 app.Pomodoro = Backbone.Model.extend({
   // Set the default values for the model properties.
   defaults: {
-    // Minutes.
-    time: 25
+    // Turn to seconds.
+    time: 25 * 60
   }
 });
 // Make the pomodoro model, so it is globally available.
--- views/pomodoro.js
+++ views/pomodoro.js
@@ -16,16 +16,27 @@ app.PomodoroView = Backbone.View.extend({
   initialize: function() {
     // Use Underscore.js to keep the context of the functions of this view tyed to
     // the view itself.
-    _.bindAll(this, "getUserTime");
+    _.bindAll(this, "render", "displayAsTimer", "getUserTime");
     // Get the input field responsible of setting the time.
     this.$input = this.$("input[type='number']");
     // Wait for model changes.
-    this.listenTo(pomodoro, "change", this.render);
+    this.listenTo(pomodoro, "change", this.displayAsTimer);
   },
 
   // Updates the HTML.
+  // Called by "this.displayAsTimer".
   render: function() {
-    this.$(".timer").html(pomodoro.get("time"));
+    this.$(".timer").html(this.displayedTime);
+  },
+
+  // Called by the "change" event fired by the model.
+  displayAsTimer: function() {
+    // Take the model time.
+    var time = pomodoro.get("time");
+    // Show it clock-like.
+    this.displayedTime = "" + Math.floor(time / 60) + " : " + Math.floor(time % 60);
+    // Call render to update the HTML.
+    this.render();
   },
 
   // Get the user time and updated the model property.
@@ -34,7 +45,8 @@ app.PomodoroView = Backbone.View.extend({
   getUserTime: function() {
     var userTime = this.$input.val();
     pomodoro.set({
-      time: userTime
+      // Turn it into seconds.
+      time: userTime * 60
     });
   }
 });
